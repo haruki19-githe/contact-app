737e00b4456e02be8f9b8fcec4d81e13814bdc9c
haruki19-githe <haruki_0120_fkt@icloud.com>
2025-07-16 17:46:02 +0900

    localhost3306に変更

--- src/main/java/output/example/contact_app/ContactAppApplication.java
+++ src/main/java/output/example/contact_app/ContactAppApplication.java
@@ -19,7 +19,7 @@ public class ContactAppApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000,https://contact-app-frontend-theta.vercel.app/") // Reactアプリが実行されるポートに合わせてください
+						.allowedOrigins("http://localhost:3000","https://contact-app-frontend-theta.vercel.app/") // Reactアプリが実行されるポートに合わせてください
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
